1e68f16e7f6e4dfbc3108eb2ae52842aab71ccb8
Steven Atkinson <atkinson.steven@gmail.com>
2018-12-01 19:03:51 -0800

    Added the searchengines patch
    This patch allows searching from the url bar using keywords. 'g'
    searches the web using google with the rest of the contents of the url
    bar. 'dd' using DuckDuckGo.

--- config.def.h
+++ config.def.h
@@ -7,6 +7,11 @@ static char *certdir        = "~/.surf/certificates/";
 static char *cachedir       = "~/.surf/cache/";
 static char *cookiefile     = "~/.surf/cookies.txt";
 
+static SearchEngine searchengines[] = {
+    { "g", "https://www.google.com/search?q=%s" },
+    { "dd", "https://duckduckgo.com/?q=%s" },
+};
+
 /* Webkit default features */
 /* Highest priority value will be used.
  * Default parameters are priority 0
